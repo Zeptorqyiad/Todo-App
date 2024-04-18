f22aef9093727752de9c2d43a5fa103584cf9923
Max Unanian <Zeptorqyiad99@gmail.com>
2024-04-19 04:03:03 +0500

    Added new functionality in App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
 import './App.css'
 import TodoForm from './components/Todos/TodoForm'
@@ -7,18 +8,37 @@ function App() {
    const [todos, setTodos] = useState([])
 
    const addTodoHandler = (text) => {
-      setTodos([...todos, text])
+      const newTodo = {
+         text,
+         isCompleted: false,
+         id: uuidv4(),
+      }
+      setTodos([...todos, newTodo])
    }
 
-   const deleteTodoHandler = (index) => {
-      setTodos(todos.filter((_, idx) => idx !== index))
+   const deleteTodoHandler = (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id))
+   }
+
+   const toggleTodoHandler = (id) => {
+      setTodos(
+         todos.map((todo) =>
+            todo.id === id
+               ? { ...todo, isCompleted: !todo.isCompleted }
+               : { todo }
+         )
+      )
    }
 
    return (
       <div className="App">
          <h1>Todo App</h1>
          <TodoForm addTodo={addTodoHandler} />
-         <TodoList todos={todos} deleteTodo={deleteTodoHandler} />
+         <TodoList
+            todos={todos}
+            deleteTodo={deleteTodoHandler}
+            toggleTodo={toggleTodoHandler}
+         />
       </div>
    )
 }
